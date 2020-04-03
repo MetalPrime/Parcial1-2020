153c520c7958fd6fd5a33dae6638c78c2c656510
Jhon Garcia <metalprime57@gmail.com>
2020-04-03 14:37:58 -0500

    first commit

---
+++ src/model/Netflix.java
@@ -0,0 +1,23 @@
+package model;
+
+import processing.core.PImage;
+
+public class Netflix implements Comparable{
+
+	public Netflix(PImage img, String name) {
+		
+	}
+
+	
+
+	
+
+
+
+	@Override
+	public int compareTo(Object o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
